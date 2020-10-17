f6cbf9d0d902392ca5210b3c82170a48b22f6a68
Daman Mohan Kumar <kumardm@uci.edu>
2020-10-16 22:47:18 -0700

    implemented command-line args, and config file parsing
    - config file needs to be manually created

---
+++ include/sadfs/sadmd/defaults.hpp
@@ -0,0 +1,20 @@
+#ifndef SADFS_SADMD_DEFAULTS_HPP
+#define SADFS_SADMD_DEFAULTS_HPP
+
+#include <string>
+
+namespace sadfs { namespace sadmd { namespace defaults {
+
+	// default configuration file path.
+	// since boost::program_options expects a const& to a
+	// std::string while setting an option's default value,
+	// this cannot be a std::string_view. if we were to use a 
+	// std::string_view, we would have to construct a std::string
+	// at runtime, which isn't appropriate for this use-case
+	std::string const config_path("/etc/sadmd.conf");
+
+} // defaults
+} // sadmd
+} // sadfs
+
+#endif // SADFS_SADMD_DEFAULTS_HPP
