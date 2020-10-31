603379fc6998fb574b9d8f109813aee6ea7ce7fa
Adam Monahan <adammonahan93@gmail.com>
2020-10-30 17:58:12 -0700

    Cleaned up sadcd. Set up struct to hold options provided by bootstrap

--- src/sadcd/sadcd.cpp
+++ src/sadcd/sadcd.cpp
@@ -7,70 +7,72 @@
 
 using namespace sadfs;
 
-void
-parse_args(int argc, char *argv[])
+//struct to hold info needed to set up a chunk server
+struct chunk_server_settings
 {
-	for (auto i = 1; i < argc - 1; i += 2)
+	std::uint16_t port;
+	void from_command_line(int, char**);
+};
+
+//Sets struct variables basd on command line arguments passed
+void chunk_server_settings::
+from_command_line(int argc, char *argv[])
+{
+	for (auto i = 1; i < argc - 1; i ++)
 	{
 		auto arg = std::string{argv[i]};
-		auto val = std::string{argv[i + 1]};
-		//Set attribute to val basd on arg
-	}
-}
-
-//Temporary
-std::uint16_t
-get_port(int argc, char *argv[])
-{
-	auto port_flag = std::string{"--port"};
-
-	for (auto i = 1; i < argc - 1; i += 2)
-	{
-		auto arg = std::string{argv[i]};
-		auto val = std::string{argv[i + 1]};
-		if (port_flag.compare(arg) == 0)
+		if (arg.compare("--port") == 0)
 		{
-			return static_cast<std::uint16_t>(std::stoi(val));
+			i++;
+			this->port = static_cast<std::uint16_t>(std::stoi(std::string{argv[i]}));
 		}
 	}
-
-	return 0;
 }
 
-//struct
+//Reads the message from a socket that just received some data
+std::string
+process_message(sadfs::socket* sock)
+{
+	auto buf = std::array<char, 512>{};
+	auto len = 0;
+	auto result = std::string{};
+	while ((len = ::read(sock->descriptor(), buf.data(), buf.size() - 1)))
+	{
+		if (len == -1)
+		{
+			std::cerr << "read error\n";
+			std::cerr << std::strerror(errno) << std::endl;
+			std::exit(1);
+		}
+		if (::write(sock->descriptor(), buf.data(), len) == -1)
+		{
+			std::cerr << "write error\n";
+			std::exit(1);
+		}
+		result += buf.data();
+		buf.fill({});
+	}
+
+	return result;
+}
 
 int 
 main(int argc, char *argv[])
 {
-	auto port = get_port(argc, argv);
-	std::cout << port << std::endl;
 
-	auto listener = inet::listener{inet::constants::ip_localhost, port};
-	auto buf = std::array<char, 10>{};
-	auto len = 0;
+	//Populate chunk_server with info from command line args
+	auto chunk_server = chunk_server_settings{};
+	chunk_server.from_command_line(argc, argv);
+
+	//Set up a listener for incoming messages
+	auto listener = inet::listener{inet::constants::ip_localhost, chunk_server.port};
 
 	while (true)
 	{
 		auto sock = listener.accept();
+		auto result = process_message(&sock);
 
-		while ((len = ::read(sock.descriptor(), buf.data(), buf.size() - 1)))
-		{
-			std::cout << len << " character read\n";
-			if (len == -1)
-			{
-				std::cerr << "read error\n";
-				std::cerr << errno << std::strerror(errno) << std::endl;
-				std::exit(1);
-			}
-			if (::write(sock.descriptor(), buf.data(), len) == -1)
-			{
-				std::cerr << "write error\n";
-				std::exit(1);
-			}
-
-			std::cout << buf.data();
-			buf.fill({});
-		}
-		std::cout << std::endl;
+		std::cout << result << std::endl;
+		// perform some action based on result
 	}
 }
