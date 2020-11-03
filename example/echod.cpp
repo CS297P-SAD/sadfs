5dee65d35b64196885570f7ff498a4a3bada9fd9
Daman Mohan Kumar <kumardm@uci.edu>
2020-11-02 19:24:58 -0800

    modified echo server behaviour to print bytes received and echoed back, instead of the actual characters

--- example/echod.cpp
+++ example/echod.cpp
@@ -15,7 +15,7 @@ main()
 	while (true)
 	{
 		auto sock = listener.accept();
-		std::cout << "accepted: " << sock.descriptor() << "\n";
+		std::cout << "accepted connection; fd: " << sock.descriptor() << "\n";
 
 		auto len = 0;
 		while ((len = ::read(sock.descriptor(), buf.data(), buf.size())))
@@ -25,18 +25,16 @@ main()
 				std::cerr << "read error\n";
 				std::exit(1);
 			}
+			std::cout << "received:    " << len << " byte(s)\n";
+
 			if (len != ::write(sock.descriptor(), buf.data(), len))
 			{
 				std::cerr << "write error\n";
 				std::exit(1);
 			}
-
-			for (auto i = 0; i < len; i++)
-			{
-				std::cout << buf[i];
-				buf[i] = {};
-			}
+			std::cout << "echoed back: " << len << " byte(s)\n";
+			buf.fill({}); // clear buffer
 		}
-		std::cout << '\n';
+		std::cout << "connection closed\n";
 	}
 }
