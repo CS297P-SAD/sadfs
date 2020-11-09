12d1808b60588b3c5b24f5ec61de2cb8fd42632a
abmono93 <35467937+abmono93@users.noreply.github.com>
2020-11-09 15:22:06 -0800

    Created sadcd class and moved chunk daemon into main.cpp file (#25)
    * Created sadcd class and moved chunk daemon into main.cpp file
    
    * fixed typo
    
    Co-authored-by: damanmkumar <59807165+damanmkumar@users.noreply.github.com>

---
+++ include/sadfs/sadcd/sadcd.hpp
@@ -0,0 +1,19 @@
+#ifndef SADFS_SADCD_SADCD_HPP
+#define SADFS_SADCD_SADCD_HPP
+#include <sadfs/comm/inet.hpp>
+
+#include <string>
+
+class sadcd
+{
+public:
+    sadfs::inet::ip_addr ip;
+    sadfs::inet::port_no port;
+    // starts server by opening a listener
+    void start();
+private:
+    // reads the message from a socket that just received some data
+    std::string process_message(sadfs::socket&);
+};
+
+#endif // SADFS_SADCD_SADCD_HPP
