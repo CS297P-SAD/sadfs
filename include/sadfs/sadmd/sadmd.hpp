7c9f673591bcd69e58dd7ded8dce5acf24113095
abmono93 <35467937+abmono93@users.noreply.github.com>
2020-11-17 23:27:47 -0800

    Create sadmd data structures, database and load database contents (#29)
    * Create sadmd data structures, database and load database contents on startup
    
    * A number of smallish changes based on Daman's review
    
    * changed chunk_server_ to be a map instead of vector
    
    * switched some ints to uint64_t
    
    * Renamed member variables and other small fixes
    
    * renamed *_space to *_chunks
    
    * Removed unnecessary variable 'res'
    
    * Changed from number_stored_chunks to chunk_count
    
    * Removed indentation in unnamed namespace
    
    * made a function private, + formatting
    
    * added a few exit statements
    
    Co-authored-by: damanmkumar <59807165+damanmkumar@users.noreply.github.com>

--- include/sadfs/sadmd/sadmd.hpp
+++ include/sadfs/sadmd/sadmd.hpp
@@ -3,9 +3,31 @@
 #include <sadfs/comm/inet.hpp>
 #include <sadfs/comm/socket.hpp>
 
+#include <chrono>
 #include <string>
+#include <sqlite3.h>
+#include <unordered_map>
+#include <vector>
 
 namespace sadfs {
+
+using chunkid = uint64_t;
+
+// all the information needed about a chunk server
+struct chunk_server_info
+{
+	inet::service service;
+	uint64_t max_chunks;
+	uint64_t chunk_count;
+	int ttl;
+};
+
+// all the information needed about a file
+struct file_info
+{
+	int ttl;
+	std::vector<chunkid> chunkids;
+};
 
 class sadmd
 {
@@ -14,11 +36,26 @@ public:
 
 	// starts server
 	void start();
+
 private:
-	// reads the message from a socket that just received some data
-	std::string process_message(sadfs::socket const&);
+	// creates (the metadata for) a new file
+	void create_file(std::string const&);
+
+	// loads file metadata from disk
+	void load_files();
+
+	// runs an sql statement on system_files_
+	void db_command(std::string const&) const noexcept;
 
 	inet::service const service_;
+	// in memory representation of each file
+	std::unordered_map<std::string, file_info> files_;
+	// metadata for each chunk server
+	std::unordered_map<uint64_t, chunk_server_info> chunk_server_metadata_;
+	// map from chunkid to list of chunk servers
+	std::unordered_map<chunkid, std::vector<chunk_server_info*> > chunk_locations_;
+	// persistent/on disk copy of files_
+	sqlite3* const files_db_;
 };
 
 } // sadfs namespace
--- src/sadmd/sadmd.cpp
+++ src/sadmd/sadmd.cpp
@@ -9,32 +9,27 @@
 #include <cstring>  // std::strerror
 #include <iostream>
 #include <string>
+#include <sqlite3.h>
 #include <unistd.h> // read/write
 
-namespace sadfs {
+namespace {
 
-sadmd::
-sadmd(char const* ip, int port) : service_(ip, port)
+sqlite3*
+open_db()
 {
-	// do nothing
-}
+	sqlite3* db;
 
-void sadmd::
-start()
-{
-	auto listener = inet::listener{service_};
-
-	while (true)
+	// open the sadmd database
+	if (sqlite3_open("sadmd.db", &db) != 0)
 	{
-		auto sock = listener.accept();
-		auto result = process_message(sock);
-
-		std::cout << result << "\n";
-		// perform some action based on result
+		std::cerr << "Error: Couldn't open database: ";
+		std::cerr << sqlite3_errmsg(db) << '\n';
+		std::exit(1);
 	}
+	return db;
 }
 
-std::string sadmd::
+std::string
 process_message(sadfs::socket const& sock)
 {
 	auto buf = std::array<char, 512>{};
@@ -61,6 +56,83 @@ process_message(sadfs::socket const& sock)
 	}
 
 	return result;
+}
+} // unnamed namespace
+
+namespace sadfs {
+
+sadmd::
+sadmd(char const* ip, int port) : service_(ip, port) , files_db_(open_db())
+{
+	// make sure the files table exists
+	db_command("CREATE TABLE IF NOT EXISTS files (filename TEXT, chunkids TEXT);");
+	// load files from the database to in-memory representation
+	load_files();
+}
+
+void sadmd::
+start()
+{
+	auto listener = inet::listener{service_};
+
+	while (true)
+	{
+		auto sock = listener.accept();
+		auto result = process_message(sock);
+
+		std::cout << result << "\n";
+		// perform some action based on result
+	}
+}
+
+void sadmd::
+create_file(std::string const& filename)
+{
+	if (!files_.count(filename))
+	{
+		files_.emplace(filename, file_info{0, });
+	}
+	else
+	{
+		// TODO: give a more meaningful error message to the user
+		std::cerr << "Error: " << filename << ": file already exists\n";
+		std::exit(1);
+	}
+	
+}
+
+void sadmd::
+db_command(std::string const& stmt) const noexcept
+{
+	if (sqlite3_exec(files_db_, stmt.c_str(), NULL, NULL, NULL) != 0)
+	{
+		std::cerr << "Error running {" << stmt << "}: ";
+		std::cerr << sqlite3_errmsg(files_db_) << '\n';
+		std::exit(1);
+	}
+}
+
+void sadmd::
+load_files()
+{
+	std::cout << "Files loaded:\n";
+	sqlite3_stmt* stmt;
+	if (sqlite3_prepare_v2(files_db_, "SELECT * FROM files;", -1, &stmt, NULL) != 0)
+	{
+		std::cerr << sqlite3_errmsg(files_db_) << '\n';
+		std::exit(1);
+	}
+
+	while (sqlite3_step(stmt) == SQLITE_ROW)
+	{
+		auto filename = std::string{reinterpret_cast<const char*>(
+			sqlite3_column_text(stmt, 0))};
+		const unsigned char* cids = sqlite3_column_text(stmt, 1);
+		std::cout << filename << ": " << cids << '\n';
+		create_file(filename);
+		//TODO: convert cids into a vector of ints and add to file_info object
+	}
+	sqlite3_finalize(stmt);
 }
 
 } // sadfs namespace
