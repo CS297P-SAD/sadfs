8595ac4c9a9b3659c71d423be8f79dc46607db6d
Daman Mohan Kumar <kumardm@uci.edu>
2020-11-18 19:59:46 -0800

    introduced chunk_request abstraction - proto-msg now sends and receives file and chunk requests

--- example/proto-msg.cpp
+++ example/proto-msg.cpp
@@ -18,32 +18,60 @@ main(int argc, char** argv)
 	using namespace sadfs;
 
 	auto sock = inet::service{inet::constants::ip_localhost, 6666}.connect();
-	auto req = comm::file_request
 	{
-		13,
-		comm::file_request::type::read,
-		{"/mnt/a/file.dat", 0, 4096}
-	};
-	if (!req.send(sock))
-	{
-		std::cerr << "error: could not send message\n";
-		std::exit(1);
+		auto req = comm::file_request
+		{
+			13,
+			comm::request_type::read,
+			{"/mnt/a/file.dat", 0, 4096}
+		};
+		if (!req.send(sock))
+		{
+			std::cerr << "error: could not send message\n";
+			std::exit(1);
+		}
+
+		auto echoed_req = comm::file_request{};
+		if (!echoed_req.recv(sock))
+		{
+			std::cerr << "error: wrong message length received\n";
+			std::exit(1);
+		}
+
+		auto const& section = echoed_req.section();
+		std::cout << "Received echoed file request..."
+			<< "\nSender id: " << echoed_req.sender()
+			<< "\nFilename:  " << section.filename
+			<< "\nOffset:    " << section.offset
+			<< "\nLength:    " << section.length
+			<< "\n";
 	}
 
-	auto echoed_req = comm::file_request{};
-	if (!echoed_req.recv(sock))
 	{
-		std::cerr << "error: wrong message length received\n";
-		std::exit(1);
-	}
+		auto req = comm::chunk_request
+		{
+			13,
+			comm::request_type::read,
+			78234
+		};
+		if (!req.send(sock))
+		{
+			std::cerr << "error: could not send message\n";
+			std::exit(1);
+		}
 
-	auto const& section = echoed_req.section();
-	std::cout << "Received echoed request..."
-	          << "\nSender id: " << echoed_req.sender()
-	          << "\nFilename:  " << section.filename
-	          << "\nOffset:    " << section.offset
-	          << "\nLength:    " << section.length
-	          << "\n";
+		auto echoed_req = comm::chunk_request{};
+		if (!echoed_req.recv(sock))
+		{
+			std::cerr << "error: wrong message length received\n";
+			std::exit(1);
+		}
+
+		std::cout << "\nReceived echoed chunk request..."
+			<< "\nSender id: " << echoed_req.sender()
+			<< "\nChunk ID:  " << echoed_req.chunk_id()
+			<< "\n";
+	}
 
 	return 0;
 }
