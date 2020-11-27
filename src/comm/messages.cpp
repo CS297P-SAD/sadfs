6e0e59a1186e39bfed5c8de87a668541e1cd4bfa
Daman Mohan Kumar <kumardm@uci.edu>
2020-11-26 17:21:08 -0800

    control messages - v2 -- WIP

--- src/comm/messages.cpp
+++ src/comm/messages.cpp
@@ -1,6 +1,7 @@
 /* implementation of control messages */
 // sadfs-specific includes
 #include <sadfs/comm/messages.hpp>
+#include <sadfs/exceptions.hpp>
 
 // standard includes
 #include <unordered_map>
@@ -8,7 +9,7 @@
 // external includes
 #include <google/protobuf/util/delimited_message_util.h>
 
-namespace sadfs { namespace comm { namespace msgs {
+namespace sadfs { namespace msgs {
 
 namespace gpio = google::protobuf::io;
 namespace gputil = google::protobuf::util;
@@ -16,14 +17,6 @@ using io_type_map = std::unordered_map<proto::io_type,
                                             io_type>;
 using proto_io_type_map = std::unordered_map<io_type,
                                             proto::io_type>;
-using host_type_map = std::unordered_map<proto::host_type,
-                                            host_type>;
-using proto_host_type_map = std::unordered_map<host_type,
-                                            proto::host_type>;
-using msg_type_map = std::unordered_map<proto::msg_type,
-                                        msg_type>;
-using proto_msg_type_map = std::unordered_map<msg_type,
-                                            proto::msg_type>;
 
 namespace {
 // handy function aliases (pointers)
@@ -32,124 +25,199 @@ auto const deserialize = gputil::ParseDelimitedFromZeroCopyStream;
 
 auto const proto_io_type_lookup = proto_io_type_map
 {
+	{io_type::unknown, proto::io_type::UNKNOWN},
 	{io_type::read, proto::io_type::READ},
 	{io_type::write, proto::io_type::WRITE},
 };
 
 auto const io_type_lookup = io_type_map
 {
+	{proto::io_type::UNKNOWN, io_type::unknown},
 	{proto::io_type::READ, io_type::read},
 	{proto::io_type::WRITE, io_type::write},
 };
 
-auto const proto_host_type_lookup = proto_host_type_map
-{
-	{host_type::unknown_host_type, proto::host_type::UNKNOWN_HOST_TYPE},
-	{host_type::client,        proto::host_type::CLIENT},
-	{host_type::chunk_server,  proto::host_type::CHUNK_SERVER},
-	{host_type::master_server, proto::host_type::MASTER_SERVER},
-};
+} // unnamed namespace
 
-auto const host_type_lookup = host_type_map
-{
-	{proto::host_type::UNKNOWN_HOST_TYPE, host_type::unknown_host_type},
-	{proto::host_type::CLIENT,        host_type::client},
-	{proto::host_type::CHUNK_SERVER,  host_type::chunk_server},
-	{proto::host_type::MASTER_SERVER, host_type::master_server},
-};
+// ========================================================
+//                      master namespace
+// ========================================================
+namespace master {
 
-auto const proto_msg_type_lookup = proto_msg_type_map
-{
-	{msg_type::unknown_msg_type, proto::msg_type::UNKNOWN_MSG_TYPE},
-	{msg_type::id,       proto::msg_type::ID},
-	{msg_type::file,     proto::msg_type::FILE},
-	{msg_type::chunk,    proto::msg_type::CHUNK},
-	{msg_type::metadata, proto::msg_type::METADATA},
-};
+using MsgCase = proto::master::control_message::MsgCase;
+using msg_type_map = std::unordered_map<MsgCase, msg_type>;
+namespace {
 
 auto const msg_type_lookup = msg_type_map
 {
-	{proto::msg_type::UNKNOWN_MSG_TYPE, msg_type::unknown_msg_type},
-	{proto::msg_type::ID,       msg_type::id},
-	{proto::msg_type::FILE,     msg_type::file},
-	{proto::msg_type::CHUNK,    msg_type::chunk},
-	{proto::msg_type::METADATA, msg_type::metadata},
+	{MsgCase::MSG_NOT_SET, msg_type::unknown},
+	{MsgCase::kFileReq,    msg_type::file_request},
 };
 
+} // unnamed namespace
+
 /* ========================================================
- *                     helper functions
+ *                      control_message 
  * ========================================================
  */
-template <typename Protobuf>
-bool
-send(gpio::ZeroCopyOutputStream* out, Protobuf const& protobuf,
-     proto::msg_id const& id)
+control_message::
+control_message(host_id id)
 {
-	// serialise the msg_id first, followed by the protobuf
-	// this way the receiver can figure out what type of a
-	// message is being sent and receive it accordingly
-	return serialize(id, out) && serialize(protobuf, out);
+	pb_.mutable_sender()->set_id(id.uuid);
 }
 
-template <typename Protobuf>
-bool
-recv(gpio::ZeroCopyInputStream* in, Protobuf& protobuf)
+msg_type control_message::
+type() const noexcept
 {
-	return deserialize(&protobuf, in, nullptr);
+	return msg_type_lookup.at(pb_.msg_case());
 }
 
-} // unnamed namespace
+bool control_message::
+send(comm::io::connection const& conn) const noexcept
+{
+	if (this->type() == msg_type::unknown)
+	{
+		// nothing to send
+		return false;
+	}
+	return serialize(pb_, conn.ostream());
+}
+
+bool control_message::
+recv(comm::io::connection const& conn) noexcept
+{
+	return deserialize(&pb_, conn.istream(), nullptr);
+}
 
 /* ========================================================
  *                       file_request
  * ========================================================
  */
 file_request::
-file_request(std::size_t sender, io_type type,
-             file_section const section)
+file_request(io_type type, file_section const& section)
+	: pb_ptr_{std::make_unique<protobuf>()}
 {
-	protobuf_.set_type(proto_io_type_lookup.at(type));
-	protobuf_.set_sender(sender);
-	protobuf_.set_filename(section.filename);
-	protobuf_.set_offset(section.offset);
-	protobuf_.set_length(section.length);
+	pb_ptr_->set_type(proto_io_type_lookup.at(type));
+	pb_ptr_->set_filename(section.filename);
+	pb_ptr_->set_offset(section.offset);
+	pb_ptr_->set_length(section.length);
 }
 
-bool file_request::
-send(gpio::ZeroCopyOutputStream* out) const noexcept
+inline bool file_request::
+is_set() const noexcept
 {
-	auto id = proto::msg_id{};
-	id.set_type(proto::msg_type::FILE);
-	return msgs::send(out, protobuf_, id);
-}
-
-bool file_request::
-recv(gpio::ZeroCopyInputStream* in) noexcept
-{
-	return msgs::recv(in, protobuf_);
-}
-
-std::size_t file_request::
-sender() const noexcept
-{
-	return protobuf_.sender();
+	return bool{pb_ptr_};
 }
 
 file_section file_request::
-section() const noexcept
+section() const
 {
-	return
+	if (is_set())
 	{
-		.filename = protobuf_.filename(),
-		.offset   = protobuf_.offset(),
-		.length   = protobuf_.length()
-	};
+		return
+		{
+			.filename = pb_ptr_->filename(),
+				.offset   = pb_ptr_->offset(),
+				.length   = pb_ptr_->length()
+		};
+	}
+	throw invalid_operation("message unavailable");
 }
 
 io_type file_request::
+type() const
+{
+	if (is_set())
+	{
+		return io_type_lookup.at(pb_ptr_->type());
+	}
+	throw invalid_operation("message unavailable");
+}
+
+// extracts a raw message from the control message that is
+// (typically) received over the wire
+template <>
+bool
+extract<file_request>(file_request& raw_msg, control_message& ctl_msg)
+{
+	if (ctl_msg.type() != file_request::msg_type)
+	{
+		// cannot extract a msg that doesn't exist
+		return false;
+	}
+	// ctl_msg currently owns the protobuf message
+	// transfer the ownership to raw_msg
+	raw_msg.pb_ptr_.reset(ctl_msg.pb_.release_file_req());
+	return true;
+}
+
+// embeds a raw message into a control message that is
+// (typically) sent over the wire
+template <>
+bool
+embed<file_request>(file_request& raw_msg, control_message& ctl_msg)
+{
+	if (!raw_msg.is_set())
+	{
+		// cannot embed a msg that hasn't been set
+		return false;
+	}
+	// raw_msg currently own the protobuf message
+	// transfer the ownership to ctl_msg
+	ctl_msg.pb_.set_allocated_file_req(raw_msg.pb_ptr_.release());
+	return true;
+}
+
+} // master namespace
+
+// ========================================================
+//                      chunk namespace
+// ========================================================
+namespace chunk {
+
+using MsgCase = proto::chunk::control_message::MsgCase;
+using msg_type_map = std::unordered_map<MsgCase, msg_type>;
+namespace {
+
+auto const msg_type_lookup = msg_type_map
+{
+	{MsgCase::MSG_NOT_SET, msg_type::unknown},
+	{MsgCase::kChunkReq,   msg_type::chunk_request},
+};
+
+} // unnamed namespace
+
+/* ========================================================
+ *                      control_message 
+ * ========================================================
+ */
+control_message::
+control_message(host_id id)
+{
+	pb_.mutable_sender()->set_id(id.uuid);
+}
+
+msg_type control_message::
 type() const noexcept
 {
-	return io_type_lookup.at(protobuf_.type());
+	return msg_type_lookup.at(pb_.msg_case());
+}
+
+bool control_message::
+send(comm::io::connection const& conn) const noexcept
+{
+	if (this->type() == msg_type::unknown)
+	{
+		// nothing to send
+		return false;
+	}
+	return serialize(pb_, conn.ostream());
+}
+
+bool control_message::
+recv(comm::io::connection const& conn) noexcept
+{
+	return deserialize(&pb_, conn.istream(), nullptr);
 }
 
 /* ========================================================
@@ -157,107 +225,73 @@ type() const noexcept
  * ========================================================
  */
 chunk_request::
-chunk_request(std::size_t sender, io_type type,
-              std::size_t chunk_id)
+chunk_request(io_type type, std::size_t chunk_id)
+	: pb_ptr_{std::make_unique<protobuf>()}
 {
-	protobuf_.set_type(proto_io_type_lookup.at(type));
-	protobuf_.set_sender(sender);
-	protobuf_.set_chunk_id(chunk_id);
+	pb_ptr_->set_type(proto_io_type_lookup.at(type));
+	pb_ptr_->set_chunk_id(chunk_id);
 }
 
-bool chunk_request::
-send(gpio::ZeroCopyOutputStream* out) const noexcept
+inline bool chunk_request::
+is_set() const noexcept
 {
-	auto id = proto::msg_id{};
-	id.set_type(proto::msg_type::CHUNK);
-	return msgs::send(out, protobuf_, id);
-}
-
-bool chunk_request::
-recv(gpio::ZeroCopyInputStream* in) noexcept
-{
-	return msgs::recv(in, protobuf_);
+	return bool{pb_ptr_};
 }
 
 std::size_t chunk_request::
-sender() const noexcept
+chunk_id() const
 {
-	return protobuf_.sender();
-}
-
-std::size_t chunk_request::
-chunk_id() const noexcept
-{
-	return protobuf_.chunk_id();
+	if (pb_ptr_)
+	{
+		return pb_ptr_->chunk_id();
+	}
+	throw invalid_operation("message unavailable");
 }
 
 io_type chunk_request::
-type() const noexcept
+type() const
 {
-	return io_type_lookup.at(protobuf_.type());
+	if (pb_ptr_)
+	{
+		return io_type_lookup.at(pb_ptr_->type());
+	}
+	throw invalid_operation("message unavailable");
 }
 
-/* ========================================================
- *                      identification
- * ========================================================
- */
-identification::
-identification(host_type type, std::size_t id)
+// extracts a raw message from the control message that is
+// (typically) received over the wire
+template <>
+bool
+extract<chunk_request>(chunk_request& raw_msg, control_message& ctl_msg)
 {
-	protobuf_.set_type(proto_host_type_lookup.at(type));
-	protobuf_.set_id(id);
+	if (ctl_msg.type() != chunk_request::msg_type)
+	{
+		// cannot extract a msg that doesn't exist
+		return false;
+	}
+	// ctl_msg currently owns the protobuf message
+	// transfer the ownership to raw_msg
+	raw_msg.pb_ptr_.reset(ctl_msg.pb_.release_chunk_req());
+	return true;
 }
 
-bool identification::
-send(gpio::ZeroCopyOutputStream* out) const noexcept
+// embeds a raw message into a control message that is
+// (typically) sent over the wire
+template <>
+bool
+embed<chunk_request>(chunk_request& raw_msg, control_message& ctl_msg)
 {
-	auto id = proto::msg_id{};
-	id.set_type(proto::msg_type::ID);
-	return msgs::send(out, protobuf_, id);
+	if (!raw_msg.is_set())
+	{
+		// cannot embed a msg that hasn't been set
+		return false;
+	}
+	// raw_msg currently own the protobuf message
+	// transfer the ownership to ctl_msg
+	ctl_msg.pb_.set_allocated_chunk_req(raw_msg.pb_ptr_.release());
+	return true;
 }
 
-bool identification::
-recv(gpio::ZeroCopyInputStream* in) noexcept
-{
-	return msgs::recv(in, protobuf_);
-}
-
-std::size_t identification::
-id() const noexcept
-{
-	return protobuf_.id();
-}
-
-host_type identification::
-type() const noexcept
-{
-	return host_type_lookup.at(protobuf_.type());
-}
-
-/* ========================================================
- *                          msg_id
- * ========================================================
- */
-
-bool msg_id::
-recv(gpio::ZeroCopyInputStream* in) noexcept
-{
-	return msgs::recv(in, protobuf_);
-}
-
-msg_type msg_id::
-type() const noexcept
-{
-	return msg_type_lookup.at(protobuf_.type());
-}
-
-bool msg_id::
-send(gpio::ZeroCopyOutputStream*) const noexcept
-{
-	// this class is designed to not be sent using this function
-	return false;
-}
-
+} // chunk namespace
 } // namespace msgs
-} // namespace comm
 } // namespace sadfs
