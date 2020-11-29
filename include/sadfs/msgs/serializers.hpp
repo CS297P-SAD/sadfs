a315bf1aa2502595753f8a0957fc004a55016762
Daman Mohan Kumar <kumardm@uci.edu>
2020-11-29 03:57:04 -0800

    implemented support for sending messages to chunk servers

--- include/sadfs/msgs/serializers.hpp
+++ include/sadfs/msgs/serializers.hpp
@@ -5,11 +5,13 @@
 #include <sadfs/msgs/channel.hpp>
 #include <sadfs/msgs/messages.hpp>
 
+// external includes
+#include <google/protobuf/util/delimited_message_util.h>
+
 namespace sadfs { namespace msgs {
 
-namespace master {
-
-// serializes messages intended for the master server
+// uses ControlMessage as a container for raw control messages
+template <typename ControlMessage>
 class serializer
 {
 public:
@@ -20,15 +22,16 @@ public:
 	bool serialize(RawMessage const&, channel const&);
 
 private:
-	proto::master::control_message cm_{};
+	ControlMessage cm_{};
 
 	friend class msgs::channel;
 	bool serialize(gpio::ZeroCopyOutputStream*) const;
 };
 
-// template definition
+// template definitions
+template <typename ControlMessage>
 template <typename RawMessage>
-bool serializer::
+bool msgs::serializer<ControlMessage>::
 serialize(RawMessage const& rm, channel const& ch)
 {
 	auto res = embed(rm, cm_) && ch.accept_serializer(*this);
@@ -37,7 +40,25 @@ serialize(RawMessage const& rm, channel const& ch)
 	return res;
 }
 
-} // master namespace
+template <typename ControlMessage>
+bool serializer<ControlMessage>::
+serialize(gpio::ZeroCopyOutputStream* out) const
+{
+	namespace gputil = google::protobuf::util;
+	auto const serialize = gputil::SerializeDelimitedToZeroCopyStream;
+
+	return serialize(cm_, out);
+}
+
+// define a serializer to send messages to the master server
+namespace master {
+using serializer = msgs::serializer<proto::master::control_message>;
+}
+
+// define a serializer to send messages to chunk servers
+namespace chunk {
+using serializer = msgs::serializer<proto::chunk::control_message>;
+}
 
 } // msgs namespace
 } // sadfs namespace
