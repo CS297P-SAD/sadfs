614b475e859197dcedcddf3c9f2275e6a63db560
Daman Mohan Kumar <kumardm@uci.edu>
2020-12-04 14:03:56 -0800

    (re)implementation of control messages
    - wrapper classes for protobufs only provide an interface
      to create control messages
    - channel class manages IO channels around a socket
    - (de)serializer classes manage (de)serialization based on recipient
    - separation of responsibilities done so that new control
      messages can be added without modifying existing code
    - serialization and deserialization of `chunk_location_request`s
      and `chunk_request`s supported

---
+++ include/sadfs/msgs/deserializers.hpp
@@ -0,0 +1,66 @@
+#ifndef SADFS_MSGS_DESERIALIZERS_HPP
+#define SADFS_MSGS_DESERIALIZERS_HPP
+
+// sadfs-specific includes
+#include <sadfs/msgs/channel.hpp>
+#include <sadfs/msgs/messages.hpp>
+
+// external includes
+#include <google/protobuf/util/delimited_message_util.h>
+
+namespace sadfs { namespace msgs {
+
+// Container refers to a container for control messages
+template <typename Container>
+class deserializer
+{
+public:
+	deserializer() = default; // TODO: remove
+	deserializer(host_id id);
+
+	template <typename ControlMessage>
+	bool deserialize(ControlMessage&, channel const&);
+
+private:
+	Container container_{};
+
+	friend class msgs::channel;
+	bool deserialize(gpio::ZeroCopyInputStream*);
+};
+
+// template definitions
+template <typename Container>
+template <typename ControlMessage>
+bool msgs::deserializer<Container>::
+deserialize(ControlMessage& cm, channel const& ch)
+{
+	auto res =  ch.accept_deserializer(*this) && extract(cm, container_);
+	// we must not hold data after serialization
+	container_.clear_msg();
+	return res;
+}
+
+template <typename Container>
+bool deserializer<Container>::
+deserialize(gpio::ZeroCopyInputStream* in)
+{
+	namespace gputil = google::protobuf::util;
+	auto const deserialize = gputil::ParseDelimitedFromZeroCopyStream;
+
+	// TODO: change API to return clean_eof
+	return deserialize(&container_, in, /*clean_eof=*/nullptr);
+}
+
+// define a deserializer to receive messages sent to the master server
+namespace master {
+using deserializer = msgs::deserializer<proto::master::message_container>;
+}
+
+// define a deserializer to receive messages sent to chunk servers
+namespace chunk {
+using deserializer = msgs::deserializer<proto::chunk::message_container>;
+}
+} // msgs namespace
+} // sadfs namespace
+
+#endif // SADFS_MSGS_DESERIALIZERS_HPP
