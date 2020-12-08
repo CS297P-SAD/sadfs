4bac3fcbd0eb569ca3b9f9ee473ac79edd86bc69
Daman Mohan Kumar <kumardm@uci.edu>
2020-12-08 12:24:57 -0800

    added .clang-format and a script to run it

---
+++ format.sh
@@ -0,0 +1,19 @@
+#! /bin/sh
+
+if ! command -v clang-format 1>/dev/null ; then
+	echo "clang-format unavailable!"
+	exit 1
+fi
+
+if [ ! -f .clang-format ] ; then
+	echo ".clang-format unavailable!"
+	exit 1
+fi
+
+if [ ! "$#" -gt 0 ] ; then
+	echo "input files missing"
+	exit 1
+fi
+
+echo "running clang-format on files: $@"
+clang-format --style=file "$@"
