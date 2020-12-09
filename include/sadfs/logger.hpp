6eab8eb3c94bfb3bc0b98bd4485d408da1e2f1b3
Daman Mohan Kumar <kumardm@uci.edu>
2020-12-09 11:00:59 -0800

    added timestamp support

--- include/sadfs/logger.hpp
+++ include/sadfs/logger.hpp
@@ -1,6 +1,8 @@
 #ifndef SADFS_LOGGER_HPP
 #define SADFS_LOGGER_HPP
 
+#include <chrono>
+#include <iomanip>
 #include <iostream>
 
 namespace sadfs
@@ -10,24 +12,36 @@ using namespace std::string_view_literals;
 namespace logger
 {
 
+namespace
+{
+auto timestamp = []() {
+    using std::chrono::system_clock;
+    auto now = system_clock::to_time_t(system_clock::now());
+    std::cout << std::put_time(std::gmtime(&now), "%c %Z");
+};
+}
+
 inline void
 info(std::string_view msg)
 {
-    std::cout << "[INFO]: " << msg << "\n";
+    timestamp();
+    std::cout << " [INFO]: " << msg << "\n";
 }
 
 inline void
 debug(std::string_view msg)
 {
 #ifndef NDEBUG
-    std::cout << "[DEBUG]: " << msg << "\n";
+    timestamp();
+    std::cout << " [DEBUG]: " << msg << "\n";
 #endif // NDEBUG
 }
 
 inline void
 error(std::string_view msg)
 {
-    std::cout << "[ERROR]: " << msg << "\n";
+    timestamp();
+    std::cout << " [ERROR]: " << msg << "\n";
 }
 
 inline void
