991d33731bd6957b052a489ab35ca1fb6f964461
Adam Monahan <adammonahan93@gmail.com>
2020-12-10 22:14:59 -0800

    Format

--- include/sadfs/sadmd/sadmd.hpp
+++ include/sadfs/sadmd/sadmd.hpp
@@ -18,116 +18,118 @@
 #include <unordered_map>
 #include <vector>
 
-namespace sadfs {
+namespace sadfs
+{
 
 using time_point = std::chrono::steady_clock::rep;
 
 // all the information needed about a chunk server
 struct chunk_server_info
 {
-	comm::service service;
-	uint64_t max_chunks;
-	uint64_t chunk_count;
-	time_point valid_until;
+    comm::service service;
+    uint64_t      max_chunks;
+    uint64_t      chunk_count;
+    time_point    valid_until;
 };
 
 // all the information needed about a file
 struct file_info
 {
-	time_point locked_until;
-	util::file_chunks chunkids;
-	uint32_t size;
+    time_point        locked_until;
+    util::file_chunks chunkids;
+    uint32_t          size;
 };
 
 // all the information needed about a chunk
 struct chunk_info
 {
-	version latest_version;
-	// all the places this chunk is stored and what version is there
-	std::unordered_map<serverid, version> locations;
+    version latest_version;
+    // all the places this chunk is stored and what version is there
+    std::unordered_map<serverid, version> locations;
 };
 
 class sadmd
 {
 public:
-	sadmd(char const* ip, int port);
+    sadmd(char const *ip, int port);
 
-	// starts server
-	void start();
+    // starts server
+    void start();
 
-	// handles a chunk_write_notification
-	bool handle(msgs::master::chunk_write_notification const&, 
-                msgs::channel const&);
+    // handles a chunk_write_notification
+    bool handle(msgs::master::chunk_write_notification const &,
+                msgs::channel const &);
 
-	// handles a chunk_location_request and responds to channel it came in on
-	bool handle(msgs::master::chunk_location_request const&, 
-                msgs::channel const&);
+    // handles a chunk_location_request and responds to channel it came in on
+    bool handle(msgs::master::chunk_location_request const &,
+                msgs::channel const &);
 
-	// handles a chunk_server_hearbeat
-	/* TODO
-	bool handle(msgs::master::chunk_server_heartbeat const&, 
-                msgs::channel const&);
-	*/
+    // handles a chunk_server_hearbeat
+    /* TODO
+    bool handle(msgs::master::chunk_server_heartbeat const&,
+            msgs::channel const&);
+    */
 
-	// handles a join_network_request and responds to channel it came in on
-	bool handle(msgs::master::join_network_request const&, 
-                msgs::channel const&);
+    // handles a join_network_request and responds to channel it came in on
+    bool handle(msgs::master::join_network_request const &,
+                msgs::channel const &);
 
-	// handles a create_file_request
-	bool handle(msgs::master::create_file_request const&, 
-                msgs::channel const&);
+    // handles a create_file_request
+    bool handle(msgs::master::create_file_request const &,
+                msgs::channel const &);
 
-	// handles a file_info_request
-	bool handle(msgs::master::file_info_request const&, 
-                msgs::channel const&);
+    // handles a file_info_request
+    bool handle(msgs::master::file_info_request const &,
+                msgs::channel const &);
+
 private:
-	// takes ownership of a channel and serves the request on it
-	void serve_requests(msgs::channel);
+    // takes ownership of a channel and serves the request on it
+    void serve_requests(msgs::channel);
 
-	// creates (the metadata for) a new file
-	bool create_file(std::string const&);
+    // creates (the metadata for) a new file
+    bool create_file(std::string const &);
 
-	// loads file metadata from disk
-	void load_files();
-	bool load_file(std::string const&, std::string const&);
+    // loads file metadata from disk
+    void load_files();
+    bool load_file(std::string const &, std::string const &);
 
-	// copies in-memory files into database
-	void save_files() const noexcept;
+    // copies in-memory files into database
+    void save_files() const noexcept;
 
-	// runs an sql statement on system_files_
-	void db_command(std::string const&) const;
+    // runs an sql statement on system_files_
+    void db_command(std::string const &) const;
 
-	// returns true if the database contains a file with the given name
-	bool db_contains(std::string const&) const;
+    // returns true if the database contains a file with the given name
+    bool db_contains(std::string const &) const;
 
-	// functions for maintaining chunk servers 
+    // functions for maintaining chunk servers
 
-	void append_chunk_to_file(std::string const&, chunkid);
+    void append_chunk_to_file(std::string const &, chunkid);
 
-	bool add_chunk_to_server(chunkid, version, serverid);
+    bool add_chunk_to_server(chunkid, version, serverid);
 
-	void reintroduce_chunks_to_network(util::file_chunks);
+    void reintroduce_chunks_to_network(util::file_chunks);
 
-	// must be a class member to have access to chunk_metadata_
-	std::vector<comm::service> valid_servers(chunk_info&, bool);
+    // must be a class member to have access to chunk_metadata_
+    std::vector<comm::service> valid_servers(chunk_info &, bool);
 
-	// returns true on success
-	bool add_server_to_network(serverid, comm::service, uint64_t, uint64_t);
-	void remove_server_from_network(serverid) noexcept;
-	void register_server_heartbeat(serverid) noexcept;
-	bool is_active(serverid) const noexcept;
+    // returns true on success
+    bool add_server_to_network(serverid, comm::service, uint64_t, uint64_t);
+    void remove_server_from_network(serverid) noexcept;
+    void register_server_heartbeat(serverid) noexcept;
+    bool is_active(serverid) const noexcept;
 
-	comm::service const service_;
-	// in memory representation of each file
-	std::unordered_map<std::string, file_info> files_;
-	// metadata for each chunk server
-	std::unordered_map<serverid, chunk_server_info> chunk_server_metadata_;
-	// map from chunkid to chunk info
-	std::unordered_map<chunkid, chunk_info> chunk_metadata_;
-	// persistent/on disk copy of files_
-	sqlite3* const files_db_;
+    comm::service const service_;
+    // in memory representation of each file
+    std::unordered_map<std::string, file_info> files_;
+    // metadata for each chunk server
+    std::unordered_map<serverid, chunk_server_info> chunk_server_metadata_;
+    // map from chunkid to chunk info
+    std::unordered_map<chunkid, chunk_info> chunk_metadata_;
+    // persistent/on disk copy of files_
+    sqlite3 *const files_db_;
 };
 
-} // sadfs namespace
+} // namespace sadfs
 
 #endif // SADFS_SADMD_SADMD_HPP
--- src/msgs/client/messages.cpp
+++ src/msgs/client/messages.cpp
@@ -1,23 +1,28 @@
 /* defines control messages intended for clients */
-//sadfs-specific includes
+// sadfs-specific includes
 #include <sadfs/comm/inet.hpp>
 #include <sadfs/msgs/client/messages.hpp>
 
 // standard includes
-#include <iterator>    // std::back_inserter
+#include <iterator> // std::back_inserter
 
-namespace sadfs { namespace msgs { namespace client {
+namespace sadfs
+{
+namespace msgs
+{
+namespace client
+{
 
 using proto::client::message_container;
-using MsgCase = message_container::MsgCase;
+using MsgCase      = message_container::MsgCase;
 using msg_type_map = std::unordered_map<MsgCase, msg_type>;
-namespace {
-
-auto const msg_type_lookup = msg_type_map
+namespace
 {
-	{MsgCase::MSG_NOT_SET,       msg_type::unknown},
-	{MsgCase::kFileInfoRes, msg_type::file_info_response},
-	{MsgCase::kChunkLocationRes, msg_type::chunk_location_response},
+
+auto const msg_type_lookup = msg_type_map{
+    {MsgCase::MSG_NOT_SET, msg_type::unknown},
+    {MsgCase::kFileInfoRes, msg_type::file_info_response},
+    {MsgCase::kChunkLocationRes, msg_type::chunk_location_response},
 };
 
 } // unnamed namespace
@@ -25,85 +30,83 @@ auto const msg_type_lookup = msg_type_map
 // ==================================================================
 //                     file_info_response
 // ==================================================================
-file_info_response::
-file_info_response(bool exists, uint64_t size)
+file_info_response::file_info_response(bool exists, uint64_t size)
 {
-	protobuf_.set_exists(exists);
-	protobuf_.set_size(size);
+    protobuf_.set_exists(exists);
+    protobuf_.set_size(size);
 }
 
 // embeds a control message into a container that is
 // (typically) sent over the wire
 bool
-embed(file_info_response const& res, message_container& cm)
+embed(file_info_response const &res, message_container &cm)
 {
-	// should this be in a try-catch block?
-	// msg.mutable_chunk_location_res() can throw if heap allocation fails
-	*cm.mutable_file_info_res() = res.protobuf_;
-	return true;
+    // should this be in a try-catch block?
+    // msg.mutable_chunk_location_res() can throw if heap allocation fails
+    *cm.mutable_file_info_res() = res.protobuf_;
+    return true;
 }
 
 // extracts a control message from a container that is
 // (typically) received over the wire
 bool
-extract(file_info_response& res, message_container const& cm)
+extract(file_info_response &res, message_container const &cm)
 {
-	if (msg_type_lookup.at(cm.msg_case()) != file_info_response::type)
-	{
-		// cannot extract a msg that doesn't exist
-		return false;
-	}
+    if (msg_type_lookup.at(cm.msg_case()) != file_info_response::type)
+    {
+        // cannot extract a msg that doesn't exist
+        return false;
+    }
 
-	// read file_info_response from message_container
-	res.protobuf_ = cm.file_info_res();
-	return true;
+    // read file_info_response from message_container
+    res.protobuf_ = cm.file_info_res();
+    return true;
 }
 
 // ==================================================================
 //                     chunk_location_response
 // ==================================================================
-chunk_location_response::
-chunk_location_response(bool ok, std::vector<comm::service> const& services, 
-                        chunkid chunk_id, version version_num)
+chunk_location_response::chunk_location_response(
+    bool ok, std::vector<comm::service> const &services, chunkid chunk_id,
+    version version_num)
 {
-	protobuf_.set_ok(ok);
-	for (auto service : services)
-	{
-		protobuf_.add_server_ips(to_string(service.ip()));
-		protobuf_.add_ports(to_int(service.port()));
-	}
-	chunk_id.serialize(std::back_inserter(*protobuf_.mutable_chunk_id()));
-	protobuf_.set_version_num(version_num);
+    protobuf_.set_ok(ok);
+    for (auto service : services)
+    {
+        protobuf_.add_server_ips(to_string(service.ip()));
+        protobuf_.add_ports(to_int(service.port()));
+    }
+    chunk_id.serialize(std::back_inserter(*protobuf_.mutable_chunk_id()));
+    protobuf_.set_version_num(version_num);
 }
 
 // embeds a control message into a container that is
 // (typically) sent over the wire
 bool
-embed(chunk_location_response const& res, message_container& cm)
+embed(chunk_location_response const &res, message_container &cm)
 {
-	// should this be in a try-catch block?
-	// msg.mutable_chunk_location_res() can throw if heap allocation fails
-	*cm.mutable_chunk_location_res() = res.protobuf_;
-	return true;
+    // should this be in a try-catch block?
+    // msg.mutable_chunk_location_res() can throw if heap allocation fails
+    *cm.mutable_chunk_location_res() = res.protobuf_;
+    return true;
 }
 
 // extracts a control message from a container that is
 // (typically) received over the wire
 bool
-extract(chunk_location_response& res, message_container const& cm)
+extract(chunk_location_response &res, message_container const &cm)
 {
-	if (msg_type_lookup.at(cm.msg_case()) != chunk_location_response::type)
-	{
-		// cannot extract a msg that doesn't exist
-		return false;
-	}
+    if (msg_type_lookup.at(cm.msg_case()) != chunk_location_response::type)
+    {
+        // cannot extract a msg that doesn't exist
+        return false;
+    }
 
-	// read chunk_location_response from message_container
-	res.protobuf_ = cm.chunk_location_res();
-	return true;
+    // read chunk_location_response from message_container
+    res.protobuf_ = cm.chunk_location_res();
+    return true;
 }
 
-
-} // client namespace
-} // msgs namespace
-} // sadfs namespace
+} // namespace client
+} // namespace msgs
+} // namespace sadfs
