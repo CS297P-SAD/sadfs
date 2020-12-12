3c13f61bf7fda23d9ca9ce3a7c3819d72246fa12
Daman Mohan Kumar <kumardm@uci.edu>
2020-12-12 13:48:35 -0800

    implemented chunk read
    - chunk server does not persist data to disk, chunk info is
      hardcoded for testing
    - added a create-chunk program that creates a 64 MB chunk
      with the format:
      <36-char UUID>00000...\n
      <36-char UUID>11111...\n
      ...
      <36-char UUID>99999...\n
      <36-char UUID>00000...\n
    - create-chunks.sh creates the chunks whose info is hardcoded
    - chunk-read-req send a few requests, some of which are rejected,
      and some of which retrieve data. Only data transfer of 64 bytes
      tested.

---
+++ example/create-chunks.sh
@@ -0,0 +1,65 @@
+#!/ bin / sh
+
+if
+    [!$ # - eq 2]; then
+	echo 2 and only 2 arguments must be passed
+	echo read "$0" for more information
+	exit 1
+fi
+
+if [ ! -x "$1" ] ;
+then         echo "$1" not an executable fi
+
+    BIN = "${PWD}/$1"
+
+              mkdir -
+              p "$2" &&
+          cd "$2"
+
+              mkdir 229217b9 -
+              6bf0 - 47f0 - 9752 - 64541a99a67a cd 229217b9 - 6bf0 - 47f0 -
+              9752 - 64541a99a67a $
+{
+    BIN
+}
+1 cd../
+
+        mkdir                            bf5b432c -
+    3889 - 4484 - aa3c - 77a08d30901b cd bf5b432c - 3889 - 4484 - aa3c -
+    77a08d30901b $
+{
+    BIN
+}
+5 cd../
+
+        mkdir 6cfe7de1 -
+    b508 - 4e26 - 864e-7887fbc700ef cd 6cfe7de1 - b508 - 4e26 -
+    864e-7887fbc700ef $
+{
+    BIN
+}
+2 cd../
+
+        mkdir                            d7e30bce -
+    1c62 - 4b9a - b88b - c9d2632142a7 cd d7e30bce - 1c62 - 4b9a - b88b -
+    c9d2632142a7                         $
+{
+    BIN
+}
+6 cd../
+
+        mkdir                            d7e30bce -
+    1c62 - 4b9a - b88b - c9d2632142a7 cd d7e30bce - 1c62 - 4b9a - b88b -
+    c9d2632142a7                         $
+{
+    BIN
+}
+7 cd../
+
+        mkdir 8cf03844 -
+    5a57 - 47e6 - ab60 - 81f2de556aa4 cd 8cf03844 - 5a57 - 47e6 - ab60 -
+    81f2de556aa4 $
+{
+    BIN
+}
+5 cd../
